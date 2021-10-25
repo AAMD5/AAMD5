202c325581f3830fab00a591170c9f587ca3a04e
AAMD5 <93133144+AAMD5@users.noreply.github.com>
2021-10-25 14:32:22 +0100

    Update EX2_COMPLETED_CODE_AA17580.py

--- EX2_COMPLETED_CODE_AA17580.py
+++ EX2_COMPLETED_CODE_AA17580.py
@@ -95,24 +95,19 @@ def Electric_field_x(u1,u2,z,x0,xN):
             
 def Electric_field_y(v1,v2,z,y0,xN):                                # v is y' and y is screen coordinate in y-direction.
             
-    E = lambda v: np.exp(((k*1j)/(2*z))*((y0 - v)**2))              # This is the same expression for Eq(4) on the exercise but in terms of y and y' which is to be integrated.
-            
-    t = 0
-                
-    dv = (v2 - v1) / N
-        
+    E = lambda v: np.exp(((k*1j)/(2*z))*((y0 - v)**2))              # This is the same expression for Eq(4) on the exercise but in terms of y and y' which is to be integrated.           
+    t = 0            
+    dv = (v2 - v1) / N    
     S = E(v1) + E(v2)
                 
     for n in range (0,N):
                 
         if n % 2 != 0 and n < N:
-            t = 4
-                
+            t = 4                
         else:
             t = 2
                    
-        S += t*E(v1 + n*dv)
-        
+        S += t*E(v1 + n*dv)        
     Electric_field_y = (dv/3)*S 
             
     return Electric_field_y
@@ -132,18 +127,14 @@ while Selected_Option != 'q':
     
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ x0 value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
         
-        x0_value = float(input("(1) Enter a value for the lower limit of x: "))
-               
-        x0 = float(x0_value)
-                
+        x0_value = float(input("(1) Enter a value for the lower limit of x: "))               
+        x0 = float(x0_value)            
         print("\nYou have entered x0 = ",x0_value)
                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ xN value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
             
-        xN_value = float(input("(2) Enter a value for the upper limit of x: "))
-                    
-        xN = float(xN_value)
-                
+        xN_value = float(input("(2) Enter a value for the upper limit of x: "))                 
+        xN = float(xN_value)           
         print("\nYou have entered xN = ",xN_value)
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Number of iterations N ~~~~~~~~~~~~~~~~~~~~~~#
@@ -158,17 +149,14 @@ while Selected_Option != 'q':
             elif float(N_value) >= 0 and float(N_value) % 2 == 0:
                 break
                     
-        N = int(N_value)
-        
-        print("\nYou have entered a value of", N_value)
-        
+        N = int(N_value)     
+        print("\nYou have entered a value of", N_value)   
         print("\nThe value of the integral (area under the curve) for sin(x) from x =", x0 ,"to x =", xN ,"is", sinx(x0,xN),".")
     
 #~~~~~~~~~~~~~~~~~~~~~~~ PART (A) AND (B) OF THE EXCERCISE ~~~~~~~~~~~~~~~~~~~#
     
     elif Selected_Option == 'b':
-        print('\nYou have selected option (b)')
-        
+        print('\nYou have selected option (b)')       
         print("\nThis option will allow you to plot a graph of relative intensity against screen coordinate x for different parameter values. You can adjust the following parameters: \n\n(1) The lower limit of the apperture x1'. \n(2) The upper limit of the apperture x2'. \n(3) The lower limit of the screen x0. \n(4) The upper limit of the screen xN. \n(5) The screen distance z. \n(6) The number of iterations N. \n\nTo obtain the Fraunhofer far-field diffraction for a single-slit graph, the recommended parameters are: \n\n(1) x1' = -1 x 10^-5 m. \n(2) x2' = 1 x 10^-5 m. \n(3) x0 = -5 x 10^-3 m. \n(4) xN = 5 x 10^-3 m. \n(5) z = 2 cm. \n(6) N >= 100. \n\nTo obtain the Frensel near-field diffraction for a single-slit graph, the recommended parameters are: \n\n(1) x1' = -40 x 10^-5 m. \n(2) x2' = 40 x 10^-5 m. \n(3) x0 = -5 x 10^-3 m. \n(4) xN = 5 x 10^-3 m. \n(5) z = 2 cm. \n(6) N >= 600.")
         
 
@@ -188,9 +176,10 @@ while Selected_Option != 'q':
         u1_value = float(input("(1) Enter a value for aperture's lower limit (x 10^-5 m): "))
        
         while u1_value != float():
+     
             if float(u1_value) > 0:
                 u1_value = input("Enter a negative number: ")
-                
+        
             elif float(u1_value) <= 0:
                 break
                                                  
@@ -203,6 +192,7 @@ while Selected_Option != 'q':
         u2_value = float(input("(2) Enter a value for aperture's upper limit (x 10^-5 m): "))
        
         while u2_value != float():
+     
             if float(u2_value) < 0:
                 u2_value= input("Enter a positive number: ")
                 
@@ -211,8 +201,7 @@ while Selected_Option != 'q':
             
         u2 = float(u2_value)*1e-5
         
-        print("\nYou have entered x2' =",u2_value,"x 10^-5 m ")
-        
+        print("\nYou have entered x2' =",u2_value,"x 10^-5 m ")      
         print("\nApperture's width = ", float(u2 - u1), "m")
          
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ x1 value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -220,14 +209,14 @@ while Selected_Option != 'q':
         x0_value = float(input("(3) Enter a value for screen's lower limit (x 10^-3 m): "))
        
         while x0_value != float():
+     
             if float(x0_value) > 0:
                 x0_value= input("Enter a negative number: ")
                 
             elif float(x0_value) <= 0:
                 break
             
-        x0 = float(x0_value)*1e-3
-        
+        x0 = float(x0_value)*1e-3      
         print("\nYou have entered x0 =",x0_value,"x 10^-3 m ")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ x2 value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -235,16 +224,15 @@ while Selected_Option != 'q':
         xN_value = float(input("(4) Enter a value for screen's upper limit (x 10^-3 m): "))
        
         while xN_value != float():
+     
             if float(xN_value) < 0:
                 xN_value= input("Enter a positive number: ")
                 
             elif float(xN_value) >= 0:
                 break
             
-        xN = float(xN_value)*1e-3
-        
-        print("\nYou have entered xN =",xN_value,"x 10^-3 m ")
-        
+        xN = float(xN_value)*1e-3       
+        print("\nYou have entered xN =",xN_value,"x 10^-3 m ")     
         print("\nScreen width =", float(xN - x0), "m")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Screen distance z ~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -252,19 +240,20 @@ while Selected_Option != 'q':
         z_value = float(input("(5) Enter the distance to the screen (cm): "))
         
         while z_value != float():
+     
             if float(z_value)<0:
                 z_value= input("Enter a positive number: ")
                 
             elif float(z_value)>=0:
                 break
             
-        z = float(z_value)*0.01
-        
+        z = float(z_value)*0.01        
         print("\nYou have entered z =",z_value,"cm")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~ Number of iterations N ~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
         
         N_value = float(input("(6) Enter a value of N (even integer): "))
+  
         while N_value != float():
             
             if float(N_value) < 0 or float(N_value) % 2 != 0:
@@ -289,8 +278,7 @@ while Selected_Option != 'q':
 #~~~~~~~~~~~~~~ PART (C) AND (D) OF THE EXERCISE: THE 2D IMAGE  ~~~~~~~~~~~~~~#
         
     elif Selected_Option == 'c':
-        print('\nYou have selected option (c)')
-        
+        print('\nYou have selected option (c)')       
         print("\nThis option will allow you to plot a 2D intensity image graph for coordinates x and y while also allowing you to choose different parameter values. You can adjust the following parameters: \n\n(1) The lower limit of the apperture x1'. \n(2) The upper limit of the apperture x2'. \n(3) The lower limit of the screen x0. \n(4) The upper limit of the screen xN. \n(5) The lower limit of the apperture y1'. \n(6) The upper limit of the apperture y2'. \n(7) The lower limit of the screen y0. \n(8) The upper limit of the screen yN. \n(9) The screen distance z. \n(10) The number of iterations N. \n\nTo obtain a far field Fraunhofer diffraction image, the recommended parameters are: \n\n(1) x1' = -1 x 10^-4 m. \n(2) x2' = 1 x 10^-4 m. \n(3) x0 >= -3.5 x 10^-4 m. \n(4) xN >= 3.5 x 10^-4 m. \n(5) y1' = -1 x 10^-4 m. \n(6) y2' = 1 x 10^-4 m. \n(7) y0 >= -3.5 x 10^-4 m. \n(8) yN >= 3.5 x 10^-4 m. \n(9) z >= 25 mm. \n(10) N >= 100.\n\nIf you enter a value of z > 40 mm, it is advisable to make the screen width bigger by varying x0, xN and y0, yN. \n\nAs for the Frensel near-field diffraction image, the recommended parameters are: \n\n(1) x1' = -1 x 10^-4 m. \n(2) x2' = 1 x 10^-4 m. \n(3) x0 = -2.5 x 10^-4 m. \n(4) xN = 2.5 x 10^-4 m. \n(5) y1' = -1 x 10^-4 m. \n(6) y2' = 1 x 10^-4 m. \n(7) y0 = -2.5 x 10^-4 m. \n(8) yN = 2.5 x 10^-4 m. \n(9) 5 <= z <= 40 mm. \n(10) N >= 100. \n\nIf you enter a value of z < 5 mm, it is advisable to decrease the screen width by varying x0,xN and y0,yN. The value of N should also be increased to reduce the effect of artefacts. However, at values of z < 1 mm, the diffraction pattern breaks and appear 'unphysical'.")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ x1' value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -298,14 +286,14 @@ while Selected_Option != 'q':
         u1_value = float(input("(1) Enter a value for aperture's lower limit in the x' direction (x 10^-4 m): "))
        
         while u1_value != float():
+     
             if float(u1_value) > 0:
                 u1_value = input("Enter a negative number: ")
                 
             elif float(u1_value) <= 0:
                 break
             
-        u1 = float(u1_value)*1e-4
-        
+        u1 = float(u1_value)*1e-4        
         print("\nYou have entered x1'= ",u1_value,"x 10^-4 m ")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ x2' value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -313,16 +301,15 @@ while Selected_Option != 'q':
         u2_value = float(input("(2) Enter a value for aperture's upper limit in the x' direction (x 10^-4 m): "))
        
         while u2_value != float():
+     
             if float(u2_value) < 0:
                 u2_value= input("Enter a positive number: ")
                 
             elif float(u2_value) >= 0:
                 break
             
-        u2 = float(u2_value)*1e-4
-        
-        print("\nYou have entered x2' =",u2_value,"x 10^-4 m ")
-        
+        u2 = float(u2_value)*1e-4        
+        print("\nYou have entered x2' =",u2_value,"x 10^-4 m ")        
         print("\nApperture's width in the x' direction =", float(u2 - u1), "m")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ x0 value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -330,14 +317,14 @@ while Selected_Option != 'q':
         x0_value = float(input("(3) Enter a value for screen's lower limit in the x direction (x 10^-4 m): "))
        
         while x0_value != float():
+     
             if float(x0_value) > 0:
                 x0_value= input("Enter a negative number: ")
                 
             elif float(x0_value) <= 0:
                 break
             
-        x0 = float(x0_value)*1e-4
-        
+        x0 = float(x0_value)*1e-4       
         print("\nYou have entered x0 =",x0_value,"x 10^-4 m ")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ xN value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -345,16 +332,15 @@ while Selected_Option != 'q':
         xN_value = float(input("(4) Enter a value for screen's upper limit in the x direction (x 10^-4 m): "))
        
         while xN_value != float():
+     
             if float(xN_value) < 0:
                 xN_value= input("Enter a positive number: ")
                 
             elif float(xN_value) >= 0:
                 break
             
-        xN = float(xN_value)*1e-4
-        
-        print("\nYou have entered xN =",xN_value,"x 10^-4 m ")
-        
+        xN = float(xN_value)*1e-4       
+        print("\nYou have entered xN =",xN_value,"x 10^-4 m ")    
         print("\nScreen width in the x direction =", float(xN - x0), "m")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ y1' value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -362,14 +348,14 @@ while Selected_Option != 'q':
         v1_value = float(input("(5) Enter a value for aperture's lower limit in the y' direction (x 10^-4 m): "))
        
         while v1_value != float():
+     
             if float(v1_value) > 0:
                 v1_value = input("Enter a negative number: ")
                 
             elif float(v1_value) <= 0:
                 break
             
-        v1 = float(v1_value)*1e-4
-        
+        v1 = float(v1_value)*1e-4     
         print("\nYou have entered y1'=",v1_value,"x 10^-4 m ")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ y2' value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -377,16 +363,15 @@ while Selected_Option != 'q':
         v2_value = float(input("(6) Enter a value for aperture's upper limit (x 10^-4 m): "))
        
         while v2_value != float():
+     
             if float(v2_value) < 0:
                 v2_value= input("Enter a positive number: ")
                 
             elif float(v2_value) >= 0:
                 break
             
-        v2 = float(v2_value)*1e-4
-        
-        print("\nYou have entered y2'=",v2_value,"x 10^-4 m ")
-        
+        v2 = float(v2_value)*1e-4        
+        print("\nYou have entered y2'=",v2_value,"x 10^-4 m ")    
         print("\nApperture's width in the y' direction= ", float(v2 - v1), "m")
     
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ y0 value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -394,14 +379,14 @@ while Selected_Option != 'q':
         y0_value = float(input("(7) Enter a value for screen's lower limit in the y direction (x 10^-4 m): "))
        
         while y0_value != float():
+     
             if float(y0_value) > 0:
                 y0_value= input("Enter a negative number: ")
                 
             elif float(y0_value) <= 0:
                 break
             
-        y0 = float(y0_value)*1e-4
-        
+        y0 = float(y0_value)*1e-4        
         print("\nYou have entered y0 =",y0_value," x 10^-4 m ")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ yN value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -409,16 +394,15 @@ while Selected_Option != 'q':
         yN_value = float(input("(8) Enter a value for screen's upper limit in the y direction (x 10^-4 m): "))
        
         while yN_value != float():
+     
             if float(yN_value) < 0:
                 yN_value= input("Enter a positive number: ")
                 
             elif float(yN_value) >= 0:
                 break
             
-        yN = float(yN_value)*1e-4
-        
-        print("\nYou have entered yN =",yN_value,"x 10^-4 m ")
-        
+        yN = float(yN_value)*1e-4        
+        print("\nYou have entered yN =",yN_value,"x 10^-4 m ")        
         print("\nScreen width in the y direction =", float(yN - y0), "m")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Screen distance z ~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -426,19 +410,20 @@ while Selected_Option != 'q':
         z_value = float(input("(9) Enter the distance to the screen (mm): "))
         
         while z_value != float():
+     
             if float(z_value)<0:
                 z_value= input("Enter a positive number: ")
                 
             elif float(z_value)>=0:
                 break
             
-        z = (float(z_value)*0.001)/2
-        
+        z = (float(z_value)*0.001)/2       
         print("\nYou have entered z =",z_value,"mm")
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~ Number of iterations N ~~~~~~~~~~~~~~~~~~~~~~~~~~~#
         
         N_value = float(input("(10) Enter a value of N (even integer): "))
+  
         while N_value != float():
             
             if float(N_value) < 0 or float(N_value) % 2 != 0:
